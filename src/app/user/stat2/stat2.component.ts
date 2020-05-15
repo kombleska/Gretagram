5abc5c8a0439ab44d958ab3e3d45d6fc4da47520
kombleska <rebbydieng@gmail.com>
2020-05-15 15:12:30 +0200

    statistiques

---
+++ src/app/user/stat2/stat2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stat',
+  templateUrl: './stat2.component.html',
+  styleUrls: ['./stat2.component.css']
+})
+export class StatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/user/stat3/stat3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stat',
+  templateUrl: './stat3.component.html',
+  styleUrls: ['./stat3.component.css']
+})
+export class StatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
