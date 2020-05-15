0bd88f5cf67f6bef2c70e80a4ce3b33d127416da
claretcamille <camille.claret@etud.univ-jfc.fr>
2020-05-15 14:42:15 +0200

    modif inf

--- src/app/user/infos/infos.component.ts
+++ src/app/user/infos/infos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/authService';
 import { AuthGuardService } from 'src/app/services/authGuardService';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-infos',
@@ -10,16 +11,46 @@ import { AuthGuardService } from 'src/app/services/authGuardService';
 })
 export class InfosComponent implements OnInit {
 
+  userForm : FormGroup;
   user: User;
+  message: string;
 
   constructor(
+    private formBuilder : FormBuilder,
     private auth : AuthService,
-    private guard: AuthGuardService
+    private guard: AuthGuardService, 
     ) { 
-      this.user = this.auth.getUser();
+      this.message = '';
     }
 
   ngOnInit() {
+    this.user = this.auth.getUser();
+    this.initForm();
+  }
+
+  initForm(){
+    this.userForm = this.formBuilder.group({
+      mail:  [this.user.mail],
+      password1:  [this.user.pw],
+      password2:  [this.user.pw],
+      phone:  [this.user.phone],
+      address:  [this.user.address]
+    });
+  }
+
+  onSubmitForm(){
+    const formValue = this.userForm.value;
+    console.log(formValue);
+    if(formValue["password1"] == formValue["password2"]){
+      const u = {
+        "mail": formValue["mail"],
+        "password": formValue["password1"],
+        "phone": formValue["phone"],
+        "address": formValue["address"]
+      };
+      this.auth.changeInfo(this.user.mail, u);
+    }
+    
   }
 
 }
