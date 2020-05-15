a4bfc94e892e475f3857e33d835c2e958acffcd2
claretcamille <camille.claret@etud.univ-jfc.fr>
2020-05-15 15:10:53 +0200

    publication user

--- src/app/user/publi/publi.component.ts
+++ src/app/user/publi/publi.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PubliService } from 'src/app/services/publiService';
+import { AuthGuardService } from 'src/app/services/authGuardService';
+import { Publi } from 'src/app/models/publi';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-publi',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PubliComponent implements OnInit {
 
-  constructor() { }
+  allPubli: Publi[];
+  user: User;
+
+  constructor(
+    private publis : PubliService,
+    private auth : AuthGuardService,
+  ) { }
 
   ngOnInit() {
+    this.user = this.auth.userId;
+    this.allPubli =[];
+
+    const all= this.publis.getAll();
+    for(var i = 0; i < all.length; i++){
+      if(all[i].user == this.user.mail){
+        this.allPubli.push(all[i]);
+      }
+    }
   }
+
+
 
 }
